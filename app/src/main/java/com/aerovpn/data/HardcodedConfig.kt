bd4fefb14666adede7a10db3697de6430483ea9b
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-06-10 23:10:20 -0300

    Configs: Cloudflare payloads for NEXUS

--- app/src/main/java/com/aerovpn/data/HardcodedConfig.kt
+++ app/src/main/java/com/aerovpn/data/HardcodedConfig.kt
@@ -8,26 +8,26 @@ import com.aerovpn.service.protocol.SSHConfig
  */
 object HardcodedConfig {
     
-    // Server 1: Cloudfront Complex Rotate (Based on screenshot)
+    // Server 1: Cloudflare Payload (Official Project NEXUS)
     val server1 = SSHConfig(
-        name = "La Clave - Cloudfront Rotate",
+        name = "La Clave - Conexión Total",
         serverAddress = "emailmarketing.personal.com.ar",
         serverPort = 80,
         username = "GUSTY",
         password = "12345",
-        payload = "COPY / HTTP/1.3[crlf]Host: [host][crlf][crlf]\n[lf][lf][instant_split][lf][lf]\nX / HTTP/1.2[crlf]Host: [host][crlf][lf][crlf]GET /app100123 HTTP/1.1[crlf]Host:[rotate=cloudfront-03.cdn-hub.org;d9l7mhbnyv6wq.cloudfront.net;d2iowtuv61tbhu.cloudfront.net][crlf]Upgrade:websocket[crlf]Connection: Upgrade[crlf][crlf]",
+        payload = "GET / HTTP/1.1[crlf]Host: emailmarketing.personal.com.ar[crlf]Upgrade: websocket[crlf]Connection: Upgrade[crlf][crlf]",
         proxyHost = "128.254.190.146",
         proxyPort = 80
     )
 
-    // Server 2: MKCOL
+    // Server 2: Cloudflare Backup
     val server2 = SSHConfig(
-        name = "La Clave - MKCOL",
+        name = "La Clave - Backup NEXUS",
         serverAddress = "emailmarketing.personal.com.ar",
         serverPort = 80,
         username = "GUSTY",
         password = "12345",
-        payload = "MKCOL /todosvienenalgrupocastigador  HTTP/1.9[lf]Host: recargas.personal.com.ar[lf]Expect: 100-continue[crlf][crlf][split][crlf][crlf]GET- // HTTP/1.1[crlf]Host: gusty2.internetgm.es[crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]",
+        payload = "CONNECT [host_port] HTTP/1.1[crlf]Host: emailmarketing.personal.com.ar[crlf][crlf]",
         proxyHost = "128.254.190.146",
         proxyPort = 80
     )
